f75d26d846006282f3446c5ef0b7c122efa889d4
Jerem-67 <jeremy67230@gmail.com>
2020-04-29 19:01:14 +0200

    supression ligne commentaire

--- public/class/WindyApi.php
+++ public/class/WindyApi.php
@@ -15,11 +15,11 @@ class WindyApi
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_TIMEOUT, 1);
         $data = curl_exec($curl);
-        if(empty($data['result']['webcams'])){
-            return "Impossible d'afficher la camera, veuillez controller l'ID de celle-ci";
-        }else {
+//        if(empty($data['result']['webcams'])){
+//            return "Impossible d'afficher la camera, veuillez controller l'ID de celle-ci";
+//        }else {
             $data = json_decode($data, true);
             return '<a name="windy-webcam-timelapse-player" data-id="' .$data["result"]["webcams"]["0"]["id"] .'" href="https://windy.com/webcams/' .$data["result"]["webcams"]["0"]["id"] .'" >' . $data["result"]["webcams"]["0"]["title"].'</a><script async type="text/javascript" src="../script/player.js"></script>';
-        }
+//        }
     }
 }
